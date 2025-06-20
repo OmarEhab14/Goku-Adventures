9c937f15273a9c88c27506b0d3ec7e0ffa690c77
OmarEhab14 <www.omarehab3710@gmail.com>
2025-06-20 15:30:09 +0300

    Goku moving

---
+++ src/entity/Player.java
@@ -0,0 +1,178 @@
+package entity;
+
+import main.GamePanel;
+import main.KeyHandler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Player extends Entity {
+    enum Direction {UP, DOWN, LEFT, RIGHT}
+
+    private GamePanel gp;
+    private KeyHandler keyH;
+    private Direction direction;
+    private Map<Direction, BufferedImage[]> movingSprites;
+    private Map<Direction, BufferedImage[]> idleSprites;
+    private boolean isMoving;
+    private long lastBlinkTime;
+    private long blinkDuration = 200000000; // Duration of blink (in nanoseconds)
+    private long blinkInterval = 2000000000; // Interval between blinks (in nanoseconds)
+    private boolean isBlinking;
+    private long currentTime;
+
+    public Player(GamePanel gp, KeyHandler keyH) {
+        this.gp = gp;
+        this.keyH = keyH;
+        this.direction = Direction.DOWN;
+        this.movingSprites = new HashMap<>();
+        this.idleSprites = new HashMap<>();
+        setDefaultValues();
+        loadPlayerImages();
+    }
+
+    public void setDefaultValues() {
+        x = 100;
+        y = 100;
+        speed = 4;
+        isMoving = false;
+        lastBlinkTime = System.nanoTime();
+        isBlinking = false;
+    }
+
+    public void loadPlayerImages() {
+        try {
+            // Moving sprites
+            movingSprites.put(Direction.UP, new BufferedImage[]{
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_up_1.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_up_2.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_up_3.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_up_4.png"))
+            });
+
+            movingSprites.put(Direction.DOWN, new BufferedImage[]{
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_down_1.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_down_2.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_down_3.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_down_4.png"))
+            });
+
+            movingSprites.put(Direction.RIGHT, new BufferedImage[]{
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_right_1.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_right_2.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_right_3.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_right_4.png"))
+            });
+
+            movingSprites.put(Direction.LEFT, new BufferedImage[]{
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_left_1.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_left_2.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_left_3.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_left_4.png"))
+            });
+
+            // Idle sprites
+            idleSprites.put(Direction.UP, new BufferedImage[]{
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_up_idle.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_up_idle.png"))
+            });
+
+            idleSprites.put(Direction.DOWN, new BufferedImage[]{
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_down_idle_1.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_down_idle_2.png"))
+            });
+
+            idleSprites.put(Direction.RIGHT, new BufferedImage[]{
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_right_idle_1.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_right_idle_2.png"))
+            });
+
+            idleSprites.put(Direction.LEFT, new BufferedImage[]{
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_left_idle_1.png")),
+                    ImageIO.read(getClass().getResourceAsStream("/player/goku_left_idle_2.png"))
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void update() {
+        boolean wasMoving = isMoving; // Track the previous movement state
+        isMoving = false;
+
+        if (keyH.upPressed) {
+            direction = Direction.UP;
+            y -= speed;
+            isMoving = true;
+        } else if (keyH.downPressed) {
+            direction = Direction.DOWN;
+            y += speed;
+            isMoving = true;
+        } else if (keyH.rightPressed) {
+            direction = Direction.RIGHT;
+            x += speed;
+            isMoving = true;
+        } else if (keyH.leftPressed) {
+            direction = Direction.LEFT;
+            x -= speed;
+            isMoving = true;
+        }
+
+        if (isMoving) {
+            if (!wasMoving) {
+                // Reset the blink timer when transitioning from idle to moving
+                lastBlinkTime = System.nanoTime();
+                spriteNum = 1; // Reset to default sprite for moving
+            }
+            spriteCounter++;
+            if (spriteCounter > 8) {
+                spriteNum = (spriteNum % 4) + 1;
+                spriteCounter = 0;
+            }
+        } else {
+            if (wasMoving) {
+                lastBlinkTime = System.nanoTime();
+                // Reset spriteNum when transitioning to idle
+                spriteNum = 1;
+            }
+            currentTime = System.nanoTime();
+            System.out.println("Current Time: " + currentTime);
+            System.out.println("Last Blink Time: " + lastBlinkTime);
+            System.out.println("Time since last blink: " + (currentTime - lastBlinkTime));
+            // Check if it's time to start blinking
+            if (!isBlinking && (currentTime - lastBlinkTime >= blinkInterval)) {
+                isBlinking = true;
+                spriteNum = 2; // Switch to blinking sprite
+                // Only update lastBlinkTime when starting a blink
+                lastBlinkTime = currentTime;
+                System.out.println("Blinking started!");
+            }
+
+            // Check if it's time to stop blinking
+            if (isBlinking && (currentTime - lastBlinkTime > blinkDuration)) {
+                isBlinking = false;
+                spriteNum = 1; // Switch back to default idle sprite
+                lastBlinkTime = currentTime; // Reset timer after blink ends
+                System.out.println("Blinking ended!");
+            }
+        }
+    }
+
+
+    public void draw(Graphics2D g2) {
+        BufferedImage image;
+
+        if (isMoving) {
+            image = movingSprites.get(direction)[spriteNum - 1];
+        } else {
+            image = idleSprites.get(direction)[spriteNum - 1];
+        }
+
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+    }
+}
---
+++ src/main/GamePanel.java
@@ -0,0 +1,120 @@
+package main;
+
+import entity.Player;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    //SCREEN SETTINGS
+    final int originalTileSize = 32; // 16x16 tiles
+    final int scale = 3;
+    public final int tileSize = originalTileSize * scale; // 48 pixels
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+    KeyHandler keyH = new KeyHandler();
+    int FPS = 60;
+    boolean showFPS = true;
+    int currentFPS;
+    Player player = new Player(this, this.keyH);
+    Thread gameThread;
+
+    public GamePanel() {
+        setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.GREEN);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+//    @Override
+//    public void run() {
+//        double drawInterval = 1000000000f / FPS; // 0.01666 seconds
+//        double nextDrawTime = System.nanoTime() + drawInterval;
+//        long timer = 0;
+//        int drawCount = 0;
+//        long lastTime = System.nanoTime();
+//        long currentTime;
+//        while (gameThread != null) {
+//            currentTime = System.nanoTime();
+//            timer += currentTime - lastTime;
+//            lastTime = System.nanoTime();
+//            // 1.UPDATE
+//            update();
+//            // 2.REPAINT
+//            repaint();
+//            drawCount++;
+//
+//            try {
+//                double remainingTime = nextDrawTime - System.nanoTime();
+//                remainingTime /= 1000000;
+//                if (remainingTime < 0) {
+//                    remainingTime = 0;
+//                }
+//                Thread.sleep((long) remainingTime);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//            nextDrawTime += drawInterval;
+//            if(timer >= 1000000000) {
+//                currentFPS = drawCount;
+//                drawCount = 0;
+//                timer = 0;
+//            }
+//        }
+//    }
+
+    //Using the delta method
+    @Override
+    public void run() {
+        double drawInterval = 1000000000f / FPS; // 0.01666 seconds
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+            if(delta >= 1) {
+                // 1.UPDATE
+                update();
+                // 2.REPAINT
+                repaint();
+                delta--;
+                drawCount++;
+            }
+
+            if(timer >= 1000000000) {
+                currentFPS = drawCount;
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+
+    public void update() {
+        player.update();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        player.draw(g2);
+        if(showFPS) {
+            g2.setColor(Color.BLACK);
+            g2.drawString("FPS: " + currentFPS, 10, 20);
+        }
+        g2.dispose();
+    }
+}
